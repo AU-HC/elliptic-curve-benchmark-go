3a3df8e52206e5d8e71d0abccbd953ba30b1e739
AU-HC <202004086@post.au.dk>
2023-04-18 15:50:51 +0200

    Done with Exponential ElGamal encryption

--- main.go
+++ main.go
@@ -1,23 +1,35 @@
 package main
 
 import (
+	"elliptic-curve-benchmark-go/ecc"
 	"elliptic-curve-benchmark-go/elgamal"
 	"fmt"
+	"math/big"
+	"time"
 )
 
 func main() {
-	/*
-		curve, _, publicKey := ecc.GenerateKeyPair()
+	// Benchmark values
+	amountOfSamples := 1000
 
-		eccTimeStart := time.Now()
-		for i := 0; i < 1000; i++ {
-			ecc.Encrypt(curve, publicKey, big.NewInt(1321233121548349453), big.NewInt(1))
-		}
+	// ElGamal Elliptic Curves
+	curve, _, publicKey := ecc.GenerateKeyPair()
+	eccTimeStart := time.Now()
+	for i := 0; i < 1000; i++ {
+		ecc.Encrypt(curve, publicKey, big.NewInt(1321233121548349453), big.NewInt(1))
+	}
+	eccTimeElapsed := time.Since(eccTimeStart)
+	informationString := fmt.Sprintf("ElGamal Elliptic Curves with %d samples took: %s", amountOfSamples, eccTimeElapsed.String())
+	fmt.Println(informationString)
 
-		eccTimeElapsed := time.Since(eccTimeStart)
-		fmt.Println(eccTimeElapsed.String())
-	*/
-
-	fmt.Println(elgamal.GenerateKey().K)
+	// Exponential ElGamal
+	elgamalTimeStart := time.Now()
+	_, elgamalPublicKey := elgamal.GenerateKey()
+	for i := 0; i < 1000; i++ {
+		elgamal.Encrypt(elgamalPublicKey, big.NewInt(1321233121548349453), big.NewInt(1))
+	}
+	elgamalTimeElapsed := time.Since(elgamalTimeStart)
+	informationString = fmt.Sprintf("Exponential ElGamal with %d samples took: %s", amountOfSamples, elgamalTimeElapsed.String())
+	fmt.Println(informationString)
 
 }
